bcf12f964d2620c52d6a0946bc815802783f2b5d
EDINSSON <edinssonalexis@ufps.edu.co>
2023-06-30 00:05:31 -0500

    Revert "SD 23235656"
    This reverts commit 6025ccf269ec753a5e0b0fca6868175eca282001.

--- Controllers/HerramientasController.cs
+++ Controllers/HerramientasController.cs
@@ -17,7 +17,7 @@ namespace ApiRealidadVirtual.Controllers
         {
             _logger = logger;
             _mapper = mapper;
-            Console.WriteLine("Esto es github 4");
+            Console.WriteLine("Esto es");
         }
 
         [HttpGet("All")]
